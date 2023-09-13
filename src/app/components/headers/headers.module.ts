bc1782a2cf577114a0a49494387a535f2cfa0b3d
730550231 <lucasmelo59@unifor.br>
2023-09-13 17:35:51 -0300

    melhorando layout home

---
+++ src/app/components/headers/headers.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
+
+@NgModule({
+  declarations: [
+    NavHeaderComponent,
+  ],
+  imports: [
+
+  ],
+  exports: [
+    NavHeaderComponent,
+  ]
+
+})
+export class HeadersModule { }
