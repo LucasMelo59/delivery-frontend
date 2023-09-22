8520c4c938958e86aed0200288328c75a784cdaf
730550231 <lucasmelo59@unifor.br>
2023-09-22 17:17:02 -0300

    ajajuste button

--- src/app/components/banners/banners.module.ts
+++ src/app/components/banners/banners.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HomeBannerComponent } from './home-banner/home-banner.component';
 import { ParallaxBannerComponent } from './parallax-banner/parallax-banner.component';
+import { SimpleBannerComponent } from './simple-banner/simple-banner.component';
 
 
 
 @NgModule({
   declarations: [
     HomeBannerComponent,
-    ParallaxBannerComponent
+    ParallaxBannerComponent,
+    SimpleBannerComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/components/banners/simple-banner/simple-banner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-banner',
+  templateUrl: './simple-banner.component.html',
+  styleUrls: ['./simple-banner.component.scss']
+})
+export class SimpleBannerComponent {
+
+}
