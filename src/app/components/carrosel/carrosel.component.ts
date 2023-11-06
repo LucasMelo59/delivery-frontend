3c50f9478da59d02e8ff72656d225ff0e0920fad
730550231 <lucasmelo59@unifor.br>
2023-11-06 17:32:20 -0300

    swiper

--- src/app/components/carrosel/carrosel.component.ts
+++ src/app/components/carrosel/carrosel.component.ts
@@ -1,24 +1,48 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription, timer } from 'rxjs';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-carrosel',
   templateUrl: './carrosel.component.html',
   styleUrls: ['./carrosel.component.scss']
 })
-export class CarroselComponent implements OnInit, OnDestroy {
-  ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
-  }
+
+
+
+export class CarroselComponent implements OnInit  {
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    const swiperEl = document.querySelector('swiper-container');
+
+  // swiper parameters
+  const swiperParams = {
+    slidesPerView: 1,
+    navigation:true,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+    on: {
+      init() {
+        // ...
+      },
+    },
+  };
+
+  // now we need to assign all parameters to Swiper element
+  Object.assign(!swiperEl, swiperParams);
+
+  // and now initialize it
+  swiperEl!.initialize();
   }
 
-  swiperConfig = {
-    slidesPerView: 3, // Número de slides visíveis ao mesmo tempo
-    spaceBetween: 30, // Espaço entre os slides
-    navigation: true, // Setas de navegação
-  };
+
 
 
 
